546fc7d0c74118ccba9876051b5092020d5593f9
bivesh0 <98143801+bivesh0@users.noreply.github.com>
2022-07-18 21:52:38 +0530

    Update Ugly_number.py

--- Ugly_number.py
+++ Ugly_number.py
@@ -1,3 +1,24 @@
+n=int(input("))
+while n%2==0:
+      n=n//2      
+while n%3==0:
+      n=n//3
+while n%5==0:
+      n=n//5
+if n==1:
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")            
+
+
+
+
+
+
+
+
+
+
 n=int(input(""))
 uglyprimes=[2,3,5]
 if n==0:
