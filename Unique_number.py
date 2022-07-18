53444a3edfd97462a015995f20f3fce23b7c24a3
bivesh0 <98143801+bivesh0@users.noreply.github.com>
2022-07-18 17:16:45 +0530

    Update Unique_number.py

--- Unique_number.py
+++ Unique_number.py
@@ -4,3 +4,19 @@ if len(setofnum)==len(num):
     print("Unique Number")
 else:
     print("Not Unique Number")
+    
+    
+    (OR)
+    
+n=input()
+r=''
+flag=0
+for i in n:
+    if i not in r:
+        r=r+i
+    else:
+        flag+=1
+if flag>=1:
+    print("Not Unique Number")
+else:
+    print("Unique Number")    
