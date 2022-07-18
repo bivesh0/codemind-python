e63879075cf3b24d6a26afd652229a9eb4e81558
bivesh0 <98143801+bivesh0@users.noreply.github.com>
2022-07-18 21:34:14 +0530

    Update perfect_square_root_or_not.py

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -4,3 +4,12 @@ if int(num**(1/2))==math.sqrt(num):
     print("True")
 else:
     print("False")
+
+    
+    
+a=int(input())
+sqroot=a**(1/2)
+if int(sqroot)==sqroot:
+    print("True")
+else:
+    print("False")
