7fb0fdcfa9784255572c6c6e86409068d07c66b6
bivesh0 <98143801+bivesh0@users.noreply.github.com>
2022-07-18 21:19:42 +0530

    Update Ugly_number.py

--- Ugly_number.py
+++ Ugly_number.py
@@ -11,3 +11,37 @@ if n==1:
     print("Ugly Number")
 else:
     print("Not Ugly Number")
+    
+    
+    
+    
+    test cases failed
+    
+n=int(input())
+if n==0:
+    print("Not Ugly Number")
+else:
+    uglyprimes=[2,3,5]
+    factors=[int(x) for x in range(1,n) if n%x==0]
+    primes=[]
+    flag=0
+    for i in range(1,n):
+        c=0
+        for j in range(1,i+1):
+            if i%j==0:
+                c+=1
+        if c==2:
+            primes.append(i)
+ 
+    primefactors=[]
+    for i in factors:
+        if i in primes:
+            primefactors.append(i)
+    for i in primefactors:
+        if i not in uglyprimes:
+            flag+=1
+            break
+    if flag==0 and len(primefactors)>0:
+        print("Ugly Number")
+    else:
+        print("Not Ugly Number")
