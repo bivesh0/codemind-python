8b082bdd4b03468bfbb9f44f4a76ad1f26812c20
bivesh0 <98143801+bivesh0@users.noreply.github.com>
2022-07-25 21:07:05 +0530

    n=input() sq=str(int(n)**2) nrev=n[::-1] sqrev=str(int(nrev)**2) if sqrev==(sq[::-1]):     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,11 +1,8 @@
-n=int(input())
-sq1=n**2
-nstr=str(n)
-nrev=int(nstr[::-1])
-sq2=nrev**2
-sq2str=str(sq2)
-sq2rev=int(sq2str[::-1])
-if sq2rev==sq1:
+n=input()
+sq=str(int(n)**2)
+nrev=n[::-1]
+sqrev=str(int(nrev)**2)
+if sqrev==(sq[::-1]):
     print("True")
 else:
     print("False")
